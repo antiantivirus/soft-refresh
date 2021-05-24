1e3d64b153da66c7d3bcb2fe4789d9a36f915011
Jack MB <jackmb@Jacks-iMac.local>
2021-05-24 10:10:48 +0100

    added tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+    colors: {
+      // black: '#363636',
+      // white: '#F9F9F9'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
